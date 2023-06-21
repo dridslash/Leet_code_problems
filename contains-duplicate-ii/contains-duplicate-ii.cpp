5473f40cc6cb2c770bc60a0a8bde742bbb0805cc
dridslash <63309639+dridslash@users.noreply.github.com>
2023-06-21 17:12:28 +0100

    Time: 289 ms (11.39%) | Memory: 80.4 MB (21.14%) - LeetSync

---
+++ contains-duplicate-ii/contains-duplicate-ii.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+         std::map<int,int>set_nb;
+        int n_size = nums.size();
+
+        for(int i = 0; i < n_size ; i++){
+                if (set_nb.count(nums[i])){
+                        if (abs(i - set_nb[nums[i]]) <= k)
+                            return true;
+                }
+                set_nb[nums[i]] = i;
+        }
+        return false;
+    }
+};
