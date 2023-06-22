e01053373c7d5aa82a0eadbf2fd584ba80a3b6ff
dridslash <63309639+dridslash@users.noreply.github.com>
2023-06-22 08:44:55 +0100

    Time: 17 ms (27.91%) | Memory: 7.4 MB (42.06%) - LeetSync

--- valid-anagram/valid-anagram.cpp
+++ valid-anagram/valid-anagram.cpp
@@ -1,19 +1,10 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        int pos;
-        if (s.size() != t.size())
-            return false;
-        else{
-            for(int i = 0 ; i < s.size(); i++){
-                pos = t.find(s[i]);
-                if (pos != std::string::npos){
-                    t.erase(t.begin() + pos);
-                }
-                else
-                    return false;
-            }
-        }
-        return true;
+        std::sort(t.begin(),t.end(),std::greater<char>());
+        std::sort(s.begin(),s.end(),std::greater<char>());
+        if (s.compare(t) == 0)
+            return true;
+        return false;
     }
 };
