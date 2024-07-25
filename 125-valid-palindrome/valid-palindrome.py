fdb042deceb6920b18388e5e71535df30a71ae80
dridslash <63309639+dridslash@users.noreply.github.com>
2024-07-25 11:25:08 +0100

    Time: 37 ms (91.15%) | Memory: 17.8 MB (30.96%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -1,18 +1,6 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        f = 0;
-        l = len(s) - 1;
-        for _ in range(len(s)):
-            if (s[f].isalnum() == False):
-                f += 1;
-                continue;
-            elif (s[l].isalnum() == False):
-                l -= 1;
-                continue;
-            if (s[f].lower() != s[l].lower()):
-                return (False);
-            f += 1;
-            l -= 1;
-        return (True);
+        concat = [chata for chata in s.lower() if (chata.isalnum())];
+        return(concat == concat[::-1]);
 
         
